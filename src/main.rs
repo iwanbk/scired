ed3329bb1fdfe9f5dc42d058b5baf4fdb893316a
Iwan BK <iwanbk@gmail.com>
2024-03-12 13:41:23 +0700

    Simplify the code and prepared statements
    - remove channels and change it with simple tokio spawn
    - use prepared statements
    - simplify the getting value from the row

--- src/main.rs
+++ src/main.rs
@@ -2,139 +2,65 @@ use tokio::net::{TcpListener, TcpStream};
 use mini_redis::{Connection, Frame};
 use bytes::Bytes;
 use scylla::{Session, SessionBuilder};
-use tokio::sync::{mpsc, oneshot};
-use tokio::sync::mpsc::{Sender,Receiver};
 use anyhow::Result;
+use std::sync::Arc;
+use scylla::prepared_statement::PreparedStatement;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let session: Session = SessionBuilder::new().known_node("127.0.0.1:9042".to_string()).build().await?;
+    let session = Arc::new(session);
 
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
-    println!("Listening...");
 
-    let (tx, mut rx) = mpsc::channel(32);
-
-    tokio::spawn(async move {
-       scylla_mgr(&mut rx, session).await;
-    });
-
+    let prepared_get = Arc::new(
+        session
+            .prepare("select value from scired.strings where key=?")
+            .await?,
+    );
 
     loop {
         let (socket, _) = listener.accept().await.unwrap();
-        let tx2 = tx.clone();
+        let sess = session.clone();
+        let prep_get = prepared_get.clone();
         tokio::spawn(async move {
-            //process(socket, db).await;
-            process_scylla(socket, tx2).await;
+            handle_scylla(socket, sess, prep_get).await;
         });
 
     }
 }
 
-
-
-async fn scylla_mgr(rx: &mut Receiver<MgrCommand>, sess: Session)  {
-    println!("running scylla mgr");
-
-    while let Some(cmd) = rx.recv().await {
-        use MgrCommand::*;
-        match cmd {
-            Get {key, resp} => {
-                let mut value = String::from("");
-                if let Some(rows) = sess.
-                    query(format!("select value from scired.strings where key='{}'", key), &[])
-                    .await.unwrap()
-                    .rows {
-                    for row in rows {
-                        let a = row.columns[0].as_ref().unwrap().as_text().unwrap();
-                        println!("isi key {} = {}", key, a);
-                        value = a.to_string();
-                    }
-                }
-                let _ = resp.send(Ok(value));
-
-                /*let value = sess.
-                    query(format!("select value from scired.strings where key='{}'", key), &[])
-                    .await.unwrap()
-                    .rows.unwrap()
-                    .first().unwrap()
-                    .columns[0].as_ref().unwrap().
-                    as_text().unwrap().to_string();*/
-            }
-            Set{key, val, resp} => {
-                let val_str = std::str::from_utf8(&val).unwrap();
-                let sci_res=
-                    sess.
-                        query("insert into scired.strings (key, value) values (?, ?)",
-                               (key, val_str))
-                   .await;
-                  //.map(|_| ())
-                  //.map_err(From::from);
-                let status = match sci_res {
-                    Err(_) => "",
-                    Ok(_) => "OK",
-                };
-                let _ = resp.send(status.to_string());
-            }
-        }
-    }
-}
-//type Responder<T> = oneshot::Sender<Result<T>>;
-type Responder<T> = oneshot::Sender<T>;
-#[derive(Debug)]
-enum MgrCommand {
-    Get {
-        key: String,
-        resp: Responder<std::result::Result<String, &'static str>>,
-    },
-    Set {
-        key: String,
-        val: Bytes,
-        resp: Responder<String>,
-    }
-}
-
-async fn process_scylla(socket: TcpStream, mgr_tx: Sender<MgrCommand>) {
+async fn handle_scylla(socket: TcpStream, sess: Arc<Session>, prep_get: Arc<PreparedStatement>) {
     use mini_redis::Command::{self, Get,Set};
-
-    // db hashmap to store the data
-    //let mut db = HashMap::new();
 
     let mut conn = Connection::new(socket);
 
     while let Some(frame) = conn.read_frame().await.unwrap() {
-        //println!("[process_scylla] GOT: {:?}", frame);
-
         let resp = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                println!("process_scylla set handler: {}", cmd.key());
-                let (resp_tx, resp_rx) = oneshot::channel();
-                let cmd = MgrCommand::Set {
-                    key:cmd.key().to_string(),
-                    val:cmd.value().clone(),
-                    resp: resp_tx,
+                let val_str = std::str::from_utf8(&cmd.value()).unwrap();
+                let sci_res= sess
+                    .query("insert into scired.strings (key, value) values (?, ?)",
+                              (cmd.key().to_string(), val_str)) // TODO: make it prepared statements
+                    .await;
+                let status = match sci_res {
+                    Err(e) => Frame::Error(e.to_string()), // TODO: make sure it follows redis protocol
+                    Ok(_) => Frame::Simple("OK".to_string()),
                 };
-                mgr_tx.send(cmd).await.unwrap();
-
-                let resp = resp_rx.await;
-                println!("set handler got : {:?}", resp);
-                println!("sending SET response to client");
-                Frame::Simple("OK".to_string())
+                status
             }
             Get(cmd) => {
-                let (resp_tx, resp_rx) = oneshot::channel();
-                let cmd = MgrCommand::Get {
-                    key:cmd.key().into(),
-                    resp:resp_tx,
-                };
-                mgr_tx.send(cmd).await.unwrap();
-
-                let resp = resp_rx.await.unwrap();
-                println!("GET handler GOT: {:?}", resp);
-
-                match resp {
-                    Ok(val) => Frame::Bulk(Bytes::from(val)),
-                    Err(_) => Frame::Null,
+              match sess.execute(&prep_get, (cmd.key().to_string(),)).await { // TODO: change to map?
+                    Ok(res) => {
+                        match res.rows.unwrap().first(){
+                            Some(row) => {
+                                let val = row.columns[0].as_ref().unwrap().as_text().unwrap().to_string();
+                                Frame::Bulk(Bytes::from(val))
+                            }
+                            None=> Frame::Null
+                        }
+                    },
+                    Err(e) => Frame::Error(e.to_string()),
                 }
             }
 
@@ -143,7 +69,6 @@ async fn process_scylla(socket: TcpStream, mgr_tx: Sender<MgrCommand>) {
         conn.write_frame(&resp).await.unwrap();
     }
 }
-
 
 
 
